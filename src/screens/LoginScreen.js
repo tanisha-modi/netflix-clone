1c59790e0e1a50dfb058b0791ace598f62408f54
tanisha <tanishamodi12@gmail.com>
2023-08-24 22:42:53 +0530

    vercel first deploy

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./LoginScreen.css";
 import logo from "../image/net.png";
+import netflix1 from "../image/netflix1.mp4";
+import netflix2 from "../image/netflix2.mp4";
 import tv from "../image/tv.png";
 import tv2 from "../image/tv2.jpg";
 import tv3 from "../image/tv3.png";
@@ -14,31 +16,35 @@ function LoginScreen() {
 
   const [show, handleShow] = useState(false);
 
-  const transitionNavBar =() =>{
-    if(window.scrollY > 100){
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
       handleShow(true);
-    }else{
+    } else {
       handleShow(false);
     }
-  }
-    useEffect(() => {
-        window.addEventListener("scroll",  transitionNavBar)
-        return () => window.removeEventListener('scroll', transitionNavBar)
-      }, []);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
 
   return (
     <div className="login">
-      <div className={`nav ${show && 'navb'}`}>
-      </div>
+      <div className={`nav ${show && "navb"}`}></div>
       <div className="loginScreen">
         <div className="loginScreen_background">
           {/* <img className="loginScreen_logo" src={logo} alt=""/> */}
           {signIn ? (
-            <img className="loginScreen_logo" onClick={() => setSignIn(false)} src={logo} alt=""/>
+            <img
+              className="loginScreen_logo"
+              onClick={() => setSignIn(false)}
+              src={logo}
+              alt=""
+            />
           ) : (
-            <img className="loginScreen_logo" src={logo} alt=""/>
+            <img className="loginScreen_logo" src={logo} alt="" />
           )}
-          
+
           {signIn ? (
             <></>
           ) : (
@@ -81,66 +87,79 @@ function LoginScreen() {
         </div>
       </div>
       <div className="Screen_body">
-          {signIn ? (
-             <></>
-          ) : (
-      <div className="containers">
-        <div className="hr"></div>
-        <div className="login_footer">
-          <div className="text">
-            <h2>Enjoy on your TV</h2>
-            <span>
-              Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
-              Blu-ray players and more.
-            </span>
+        {signIn ? (
+          <></>
+        ) : (
+          <div className="containers">
+            <div className="hr"></div>
+            <div className="login_footer">
+              <div className="text">
+                <h2>Enjoy on your TV</h2>
+                <span>
+                  Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+                  Blu-ray players and more.
+                </span>
+              </div>
+              <div className="video-container">
+                <div className="image-overlay">
+                  <img src={tv} alt="Overlay" />
+                  <video autoPlay loop muted className="video-bg">
+                    <source src={netflix1} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+              </div>
+            </div>
+            <div className="hr"></div>
+            <div className="login_footer rev">
+              <div className="img">
+                <img src={tv2} alt="" />
+              </div>
+              <div className="text">
+                <h2>Download your shows to watch offline</h2>
+                <span>
+                  Save your favourites easily and always have something to
+                  watch.
+                </span>
+              </div>
+            </div>
+            <div className="hr"></div>
+            <div className="login_footer">
+              <div className="text">
+                <h2>Watch everywhere</h2>
+                <span>
+                  Stream unlimited movies and TV shows on your phone, tablet,
+                  laptop, and TV.
+                </span>
+              </div>
+              <div className="video-container">
+                <div className="image-overlay">
+                  <img src={tv3} alt="Overlay" />
+                  <video autoPlay loop muted className="video-bg2">
+                    <source src={netflix2} type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+              </div>
+            </div>
+            <div className="hr"></div>
+            <div className="login_footer rev">
+              <div className="img">
+                <img src={tv4} alt="" />
+              </div>
+              <div className="text">
+                <h2>Create profiles for kids</h2>
+                <span>
+                  Send children on adventures with their favourite characters in
+                  a space made just for them—free with your membership.
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="img">
-   <img className="tv" src={tv} alt="Base" />
-</div>
-        </div>
-        <div className="hr"></div>
-        <div className="login_footer rev">
-          <div className="img">
-            <img src={tv2} alt="" />
-          </div>
-          <div className="text">
-            <h2>Download your shows to watch offline</h2>
-            <span>
-              Save your favourites easily and always have something to watch.
-            </span>
-          </div>
-        </div>
-        <div className="hr"></div>
-        <div className="login_footer">
-          <div className="text">
-            <h2>Watch everywhere</h2>
-            <span>
-              Stream unlimited movies and TV shows on your phone, tablet,
-              laptop, and TV.
-            </span>
-          </div>
-          <div className="img">
-            <img className="tv3" src={tv3} alt="" />
-          </div>
-        </div>
-        <div className="hr"></div>
-        <div className="login_footer rev">
-          <div className="img">
-            <img src={tv4} alt="" />
-          </div>
-          <div className="text">
-            <h2>Create profiles for kids</h2>
-            <span>
-              Send children on adventures with their favourite characters in a
-              space made just for them—free with your membership.
-            </span>
-          </div>
-        </div>
+        )}
       </div>
-       )}
-       </div>
       <div className="hr"></div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
